44039a7b5145b8da25b57cb7e2414d20be5c9e86
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2024-04-03 17:18:17 -0600

    Add TagController and route for tags

--- BACKEND/routes/api.php
+++ BACKEND/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Public Routes
+Route::get('/tags', [TagController::class, 'index']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
