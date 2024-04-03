7fd2fe0e34d11f23d673dc00e39cefed83ec3368
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2024-04-03 17:53:19 -0600

    Add test route to API

--- BACKEND/routes/api.php
+++ BACKEND/routes/api.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+  return response()->json(['message' => 'It works!']);
+});
+
+// Auth & Registration Routes
+
 // Public Routes
 Route::get('/tags', [TagController::class, 'index']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
